9d25e16c6821e2ce415c13622ddc21f2107b9722
Dinesh Pandiyan <flexdinesh@gmail.com>
2026-06-24 22:38:24 +1000

    feat(web): dockerise web app

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 8088,
   },
   plugins: [tanstackStart(), nitro(), viteReact(), tailwindcss()],
 });
